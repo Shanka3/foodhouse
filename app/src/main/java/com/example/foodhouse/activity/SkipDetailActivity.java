e8cf9a038d6fb849f4396738fa12807c7994ecae
Shanka3 <shashanksrao2000@gmail.com>
2020-08-30 14:37:34 +0530

    food house

--- app/src/main/java/com/example/foodhouse/activity/SkipDetailActivity.java
+++ app/src/main/java/com/example/foodhouse/activity/SkipDetailActivity.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.foodhouse.R;
+import com.example.foodhouse.adapter.CommentAdapter;
+import com.example.foodhouse.model.Comment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SkipDetailActivity extends AppCompatActivity {
 
+    RecyclerView mCommentRecyclerView;
+    List<Comment> mComment;
+
     TextView foodDescription,foodName;
     ImageView foodImage;
+    DatabaseReference databaseReference;
+    ValueEventListener eventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +44,8 @@ public class SkipDetailActivity extends AppCompatActivity {
         foodDescription = (TextView)findViewById(R.id.txtDescription);
         foodImage = (ImageView)findViewById(R.id.ivImage2);
 
+        mCommentRecyclerView = (RecyclerView)findViewById(R.id.rv_Comment);
+
         Bundle bundle = getIntent().getExtras();
 
         if (bundle!=null){
@@ -37,6 +57,38 @@ public class SkipDetailActivity extends AppCompatActivity {
                     .load(bundle.getString("Image"))
                     .into(foodImage);
         }
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(SkipDetailActivity.this,1);
+        mCommentRecyclerView.setLayoutManager(gridLayoutManager);
+
+        mComment = new ArrayList<>();
+
+        final CommentAdapter commentAdapter = new CommentAdapter(SkipDetailActivity.this,mComment);
+        mCommentRecyclerView.setAdapter(commentAdapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Comments");
+
+        eventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // mComment.clear();
+
+                for (DataSnapshot itemSnapshot: snapshot.getChildren()){
+
+                    Comment comment = itemSnapshot.getValue(Comment.class);
+
+                    mComment.add(comment);
+                }
+
+                commentAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/foodhouse/activity/Upload_Activity.java
+++ app/src/main/java/com/example/foodhouse/activity/Upload_Activity.java
@@ -28,14 +28,15 @@ import com.google.firebase.storage.UploadTask;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.regex.Pattern;
 
 public class Upload_Activity extends AppCompatActivity {
 
-   private ImageView recipeImage;
-   private Uri uri;
-   private EditText txt_name,txt_description;
-   private String imageUrl;
-   private String publisher;
+    private ImageView recipeImage;
+    private Uri uri;
+    private EditText txt_name, txt_description;
+    private String imageUrl;
+    private String publisher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +44,9 @@ public class Upload_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_upload_activity);
         getSupportActionBar().setTitle("Upload Your Recipes");
 
-        recipeImage = (ImageView)findViewById(R.id.iv_foodImage);
-        txt_name = (EditText)findViewById(R.id.txt_Recipe_Name);
-        txt_description = (EditText)findViewById(R.id.txt_Description);
+        recipeImage = (ImageView) findViewById(R.id.iv_foodImage);
+        txt_name = (EditText) findViewById(R.id.txt_Recipe_Name);
+        txt_description = (EditText) findViewById(R.id.txt_Description);
 
     }
 
@@ -53,7 +54,7 @@ public class Upload_Activity extends AppCompatActivity {
 
         Intent photoPicker = new Intent(Intent.ACTION_PICK);
         photoPicker.setType("image/*");
-        startActivityForResult(photoPicker,1);
+        startActivityForResult(photoPicker, 1);
 
     }
 
@@ -61,17 +62,16 @@ public class Upload_Activity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK){
+        if (resultCode == RESULT_OK) {
 
             uri = data.getData();
             recipeImage.setImageURI(uri);
 
-        }
-        else Toast.makeText(this, "You Haven't Picked Any Image", Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(this, "You Haven't Picked Any Image", Toast.LENGTH_SHORT).show();
     }
 
 
-    public void uploadImage(){
+    public void uploadImage() {
 
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("RecipeImage").child(uri.getLastPathSegment());
 
@@ -84,7 +84,7 @@ public class Upload_Activity extends AppCompatActivity {
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
                 Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                while(!uriTask.isComplete());
+                while (!uriTask.isComplete()) ;
                 Uri urlImage = uriTask.getResult();
                 imageUrl = urlImage.toString();
                 uploadRecipe();
@@ -101,29 +101,34 @@ public class Upload_Activity extends AppCompatActivity {
     public void btnUploadRecipe(View view) {
         String upName = txt_name.getText().toString().trim();
         String upDescription = txt_description.getText().toString().trim();
+        Pattern regex = Pattern.compile("[$&+,:;=\\\\?@#|'<>.^*()%!-]");
 
         if (uri == null) {
-            Toast.makeText(this, "Image Not Selected", Toast.LENGTH_LONG).show();
-        }else if (TextUtils.isEmpty(upName)){
+            Toast.makeText(this, "Image Not Selected", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(upName)) {
             txt_name.setError("Recipe Name Is Empty");
             return;
-        }else if (TextUtils.isEmpty(upDescription)){
+        } else if (regex.matcher(upName).find()) {
+            txt_name.setError("SpecialCharacters Not Allowed");
+            return;
+        } else if (TextUtils.isEmpty(upDescription)) {
             txt_description.setError("Description Is Empty");
             return;
+        } else if (regex.matcher(upDescription).find()) {
+            txt_description.setError("SpecialCharacters Not Allowed");
+            return;
         } else uploadImage();
-
-
     }
 
     public void uploadRecipe() {
 
         publisher = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-       FoodData foodData = new FoodData(
-               txt_name.getText().toString(),
-               txt_description.getText().toString(),
-               imageUrl,
-               publisher
+        FoodData foodData = new FoodData(
+                txt_name.getText().toString(),
+                txt_description.getText().toString(),
+                imageUrl,
+                publisher
 
         );
 
